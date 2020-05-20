48b01d112cebd1df1f85c083e023c06fcbdd0b35
cdougher89 <63210749+cdougher89@users.noreply.github.com>
2020-05-20 14:20:17 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function Validate() {
+  if (
+    document.getElementById("keyword").value == "Octopi" &&
+    document.getElementById("number").value ==
+      document.getElementById("extra").value
+  ) {
+    document.getElementById("result").value = "HAGS HAGS HAGS";
+  } else {
+    document.getElementById("result").value = "Incorrect";
+  }
+}
+
+document.getElementById("submit").addEventListener("click", Validate);
